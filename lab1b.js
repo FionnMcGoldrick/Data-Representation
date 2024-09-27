0717f8e4dfcba6c14019b574f326c04b146eb5ef
Fionn McGoldrick <G00422349@atu.ie>
2024-09-27 11:00:32 +0000

    Lab1 a,b,c done

---
+++ lab1b.js
@@ -0,0 +1,8 @@
+let a = (sentence)=>{
+
+    console.log(sentence);
+}
+
+let b = "hello world";
+
+a(b);
---
+++ lab1c.js
@@ -0,0 +1,9 @@
+let answer = (sum1,sum2)=> {
+
+    console.log(sum1+sum2);
+
+}
+
+let sum1 = 10, sum2 = 20;
+
+answer(sum1,sum2);
